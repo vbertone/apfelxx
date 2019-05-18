b16259c77b53d5ebcbd93808660ab0ad0b9f0786
Valerio Bertone <valerio.bertone@cern.ch>
2019-05-18 14:50:28 +0200

    small adjustment

--- resources/astyle.sh
+++ resources/astyle.sh
@@ -2,4 +2,4 @@
 
 astyle --recursive --suffix=none --style=gnu --indent=spaces=2 --indent-namespaces --max-continuation-indent=120 "../src/*.cc"
 astyle --recursive --suffix=none --style=gnu --indent=spaces=2 --indent-namespaces --max-continuation-indent=120 "../tests/*.cc"
-astyle --recursive --suffix=none --style=gnu --indent=spaces=2 --indent-namespaces --keep-one-line-blocks --max-continuation-indent=120 "../inc/apfel/*.h"
+astyle --recursive --suffix=none --style=gnu --indent=spaces=2 --indent-namespaces --max-continuation-indent=120 --keep-one-line-blocks "../inc/apfel/*.h"
