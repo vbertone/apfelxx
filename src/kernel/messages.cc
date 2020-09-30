f91295b7028296530ffb932c8ae35a81cde757d2
Valerio Bertone <valerio.bertone@cern.ch>
2020-09-30 10:20:42 +0200

    Changing name of the colour code enumerator

--- src/kernel/messages.cc
+++ src/kernel/messages.cc
@@ -26,7 +26,7 @@ namespace apfel
   /**
    * @brief Colour codes
    */
-  enum code {red = 31, green = 32, yellow = 33, blue = 34, normal = 39};
+  enum ColourCode {red = 31, green = 32, yellow = 33, blue = 34, normal = 39};
 
   //_________________________________________________________________________
   void SetVerbosityLevel(int const& vl)
@@ -51,21 +51,21 @@ namespace apfel
   void info(std::string const& tag, std::string const& what)
   {
     if (VerbosityLevel > MEDIUM)
-      std::cout << "\033[" << code::blue << "m[apfel::" << tag << "] Info: " << what << "\033[" << code::normal << "m\n";
+      std::cout << "\033[" << ColourCode::blue << "m[apfel::" << tag << "] Info: " << what << "\033[" << ColourCode::normal << "m\n";
   }
 
   //_________________________________________________________________________
   void warning(std::string const& tag, std::string const& what)
   {
     if (VerbosityLevel > LOW)
-      std::cout << "\033[" << code::yellow << "m[apfel::" << tag << "] Warning: " << what << "\033[" << code::normal << "m\n";
+      std::cout << "\033[" << ColourCode::yellow << "m[apfel::" << tag << "] Warning: " << what << "\033[" << ColourCode::normal << "m\n";
   }
 
   //_________________________________________________________________________
   std::string error(std::string const& tag, std::string const& what)
   {
     std::stringstream ss;
-    ss << "\n\n\033[" << code::red << "m[apfel::" << tag << "] Error: " << what << "\033[" << code::normal << "m\n";
+    ss << "\n\n\033[" << ColourCode::red << "m[apfel::" << tag << "] Error: " << what << "\033[" << ColourCode::normal << "m\n";
     return ss.str();
   }
 
@@ -74,16 +74,16 @@ namespace apfel
   {
     if (VerbosityLevel > MEDIUM)
       {
-        std::cout << "\033[1;" << code::red << "m\n";
+        std::cout << "\033[1;" << ColourCode::red << "m\n";
         std::cout << "       _/_/_/   _/_/_/_/  _/_/_/_/  _/_/_/_/  _/\n";
         std::cout << "     _/    _/  _/    _/  _/        _/        _/        _/     _/\n";
         std::cout << "    _/_/_/_/  _/_/_/_/  _/_/_/    _/_/_/    _/      _/_/_/ _/_/_/\n";
         std::cout << "   _/    _/  _/        _/        _/        _/        _/     _/\n";
         std::cout << "  _/    _/  _/        _/        _/_/_/_/  _/_/_/_/\n";
-        std::cout << "\033[0;" << code::green << "m\n";
+        std::cout << "\033[0;" << ColourCode::green << "m\n";
         std::cout << "_____v" << VERSION << ": A PDF evolution library in C++, arXiv:1708.00911\n";
         std::cout << "     Author: V. Bertone\n";
-        std::cout << "\033[" << code::normal << "m\n";
+        std::cout << "\033[" << ColourCode::normal << "m\n";
       }
   }
 }
