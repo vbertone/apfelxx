22ee0a73af1c6dfec871e5f99b9cffe2cb411b78
vbertone <valerio.bertone@gmail.com>
2022-01-08 18:34:13 +0100

    Updating documentation

--- docs/html/search/classes_7.js
+++ docs/html/search/classes_7.js
@@ -5,5 +5,6 @@ var searchData=
   ['matchedevolution_3c_20set_3c_20t_20_3e_20_3e_2',['MatchedEvolution&lt; Set&lt; T &gt; &gt;',['../classapfel_1_1MatchedEvolution.html',1,'apfel']]],
   ['matchingbasisqcd_3',['MatchingBasisQCD',['../classapfel_1_1MatchingBasisQCD.html',1,'apfel']]],
   ['matchingoperatorbasisqcd_4',['MatchingOperatorBasisQCD',['../classapfel_1_1MatchingOperatorBasisQCD.html',1,'apfel']]],
-  ['matrix_5',['matrix',['../classapfel_1_1matrix.html',1,'apfel']]]
+  ['matrix_5',['matrix',['../classapfel_1_1matrix.html',1,'apfel']]],
+  ['matrix_3c_20double_20_3e_6',['matrix&lt; double &gt;',['../classapfel_1_1matrix.html',1,'apfel']]]
 ];
--- docs/html/search/classes_e.js
+++ docs/html/search/classes_e.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['tabulateobject_0',['TabulateObject',['../classapfel_1_1TabulateObject.html',1,'apfel']]],
   ['term_1',['term',['../structapfel_1_1term.html',1,'apfel']]],
-  ['timer_2',['Timer',['../classapfel_1_1Timer.html',1,'apfel']]],
-  ['tmdobjects_3',['TmdObjects',['../structapfel_1_1TmdObjects.html',1,'apfel']]],
-  ['twobodyphasespace_4',['TwoBodyPhaseSpace',['../classapfel_1_1TwoBodyPhaseSpace.html',1,'apfel']]]
+  ['term_3c_20t_2c_20t_20_3e_2',['term&lt; T, T &gt;',['../structapfel_1_1term.html',1,'apfel']]],
+  ['timer_3',['Timer',['../classapfel_1_1Timer.html',1,'apfel']]],
+  ['tmdobjects_4',['TmdObjects',['../structapfel_1_1TmdObjects.html',1,'apfel']]],
+  ['twobodyphasespace_5',['TwoBodyPhaseSpace',['../classapfel_1_1TwoBodyPhaseSpace.html',1,'apfel']]]
 ];
