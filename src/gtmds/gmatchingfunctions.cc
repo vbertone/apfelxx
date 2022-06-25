94c6a71d3feaaba379d90142552ce99d303ac1de
vbertone <valerio.bertone@gmail.com>
2022-06-25 18:45:32 +0200

    Adjusting Cgtmd1qg and Cgtmd1gg

--- src/gtmds/gmatchingfunctions.cc
+++ src/gtmds/gmatchingfunctions.cc
@@ -57,8 +57,24 @@ namespace apfel
     const double ky2   = pow(kappa * y, 2);
     //return (y <= 1 ? 8 * TR * y * ( 1 - y ) / pow(1 - ky2, 2) : 0)
     //       + (kappa > 1 ? 8 * TR * ( 1 - kappa ) * pow(y, 2) / pow(1 - ky2, 2) : 0);
-    return (y <= 1 ? 8 * TR * y * ( 1 - y ) / pow(1 - ky2, 1) : 0)
-           + (kappa > 1 ? 8 * TR * ( 1 - kappa ) * pow(y, 2) / pow(1 - ky2, 1) : 0);
+
+    // I multiplied this matching function by ( 1 - kappa * y )
+    // because otherwise the divergence at y = 1/kappa would not
+    // cancel and the integrals would diverge. This is probably a
+    // signal of the fact that the specific definition of the gluon
+    // GPD that is being used here vanishes at y = xi. This is visible
+    // from Eq. (116) of https://arxiv.org/pdf/2206.01412.pdf.
+    // Thefore, the additional factor ( 1 - kappa * y ) makes this
+    // matching function useable with any gluon GPD that does not
+    // vanish at y = xi and effectively amounts to replacing in the
+    // convolution integral:
+    //
+    //  G(y, xi) -> y / ( y - xi ) G(y, xi)
+    //
+    // that is assumed to be finite at y = xi. The same applies to Cgg
+    // below.
+    return ( 1 - kappa * y ) * ( (y <= 1 ? 8 * TR * y * ( 1 - y ) / pow(1 - ky2, 2) : 0)
+                                 + (kappa > 1 ? 8 * TR * ( 1 - kappa ) * pow(y, 2) / pow(1 - ky2, 2) : 0) );
   }
 
   //_________________________________________________________________________________
@@ -87,8 +103,10 @@ namespace apfel
     const double ky2   = pow(kappa * y, 2);
     //return (y <= 1 ? 8 * CA * pow(kappa, 2) * y * ( 1 - y ) / pow(1 - ky2, 2) : 0)
     //       + (kappa > 1 ? CA * ( 1 - kappa ) * ( 1 + kappa - ( 1 - 7 * kappa ) * ky2 ) / kappa / pow(1 - ky2, 2) : 0);
-    return (y <= 1 ? 8 * CA * pow(kappa, 2) * y * ( 1 - y ) / pow(1 - ky2, 1) : 0)
-           + (kappa > 1 ? CA * ( 1 - kappa ) * ( 1 + kappa - ( 1 - 7 * kappa ) * ky2 ) / kappa / pow(1 - ky2, 1) : 0);
+
+    // See Cqg above
+    return ( 1 - kappa * y ) * ( (y <= 1 ? 8 * CA * pow(kappa, 2) * y * ( 1 - y ) / pow(1 - ky2, 2) : 0)
+                                 + (kappa > 1 ? CA * ( 1 - kappa ) * ( 1 + kappa - ( 1 - 7 * kappa ) * ky2 ) / kappa / pow(1 - ky2, 2) : 0) );
   }
   double Cgtmd1gg::Local(double const&) const
   {
