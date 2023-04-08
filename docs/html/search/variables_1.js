b6b5f727dd3379b4cbd82f4f05beec8a2aded091
Valerio Bertone <valerio.bertone@cern.ch>
2023-04-08 22:01:45 +0200

    Updating documentation

--- docs/html/search/variables_1.js
+++ docs/html/search/variables_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['alphaqcdref_0',['AlphaQCDRef',['../structapfel_1_1EvolutionSetup.html#a3a404166e6945e59a218908e780382ea',1,'apfel::EvolutionSetup']]],
-  ['alphaqedref_1',['AlphaQEDRef',['../structapfel_1_1EvolutionSetup.html#a3b717826fcca628ecdebb04619161fee',1,'apfel::EvolutionSetup']]]
+  ['alphaem_0',['alphaem',['../group__PhysConstants.html#gace06aa81390640a1ea9065f6b1f43a36',1,'apfel']]],
+  ['alphaqcdref_1',['AlphaQCDRef',['../structapfel_1_1EvolutionSetup.html#a3a404166e6945e59a218908e780382ea',1,'apfel::EvolutionSetup']]],
+  ['alphaqedref_2',['AlphaQEDRef',['../structapfel_1_1EvolutionSetup.html#a3b717826fcca628ecdebb04619161fee',1,'apfel::EvolutionSetup']]]
 ];
