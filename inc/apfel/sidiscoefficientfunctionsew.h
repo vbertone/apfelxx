3a66726994a42cf8f4e898c1a602a7c6d5d63da9
vbertone <valerio.bertone@cern.ch>
2025-07-11 18:01:31 +0200

    Uploading EW SIDIS NNLO coefficient functions (still to be linked)

---
+++ inc/apfel/sidiscoefficientfunctionsew.h
@@ -0,0 +1,643 @@
+//
+// APFEL++ 2017
+//
+// Author:Valerio Bertone:valerio.bertone@cern.ch
+//
+
+#pragma once
+
+#include "apfel/doubleexpression.h"
+
+namespace apfel
+{
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC0q2qM: public DoubleExpression
+  {
+  public:
+    FTC0q2qM();
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC1q2qM: public DoubleExpression
+  {
+  public:
+    FTC1q2qM();
+    double LocalLocal(double const& x, double const& z) const override;
+    double LocalSingular(double const& x, double const& z) const override;
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularLocal(double const& x, double const& z) const override;
+    double SingularSingular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC1q2gM: public DoubleExpression
+  {
+  public:
+    FTC1q2gM();
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC1g2qM: public DoubleExpression
+  {
+  public:
+    FTC1g2qM();
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2qM: public DoubleExpression
+  {
+  public:
+    FTC2q2qM(int const& nf);
+    double LocalLocal(double const& x, double const& z) const override;
+    double LocalSingular(double const& x, double const& z) const override;
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularLocal(double const& x, double const& z) const override;
+    double SingularSingular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  private:
+    int const _nf;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2qMFcon1: public DoubleExpression
+  {
+  public:
+    FTC2q2qMFcon1();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2qMFcon2: public DoubleExpression
+  {
+  public:
+    FTC2q2qMFcon2();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2qMANoW: public DoubleExpression
+  {
+  public:
+    FTC2q2qMANoW();
+    double LocalLocal(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2gM: public DoubleExpression
+  {
+  public:
+    FTC2q2gM();
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2gMANoW: public DoubleExpression
+  {
+  public:
+    FTC2q2gMANoW();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2g2qM: public DoubleExpression
+  {
+  public:
+    FTC2g2qM();
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2g2qMANoW: public DoubleExpression
+  {
+  public:
+    FTC2g2qMANoW();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2g2gM: public DoubleExpression
+  {
+  public:
+    FTC2g2gM();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2g2gMAANoW: public DoubleExpression
+  {
+  public:
+    FTC2g2gMAANoW();
+    double LocalLocal(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2qpM1: public DoubleExpression
+  {
+  public:
+    FTC2q2qpM1();
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2qpM2: public DoubleExpression
+  {
+  public:
+    FTC2q2qpM2();
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2qpMNoW3: public DoubleExpression
+  {
+  public:
+    FTC2q2qpMNoW3();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2qpMNoW4: public DoubleExpression
+  {
+  public:
+    FTC2q2qpMNoW4();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2qbM: public DoubleExpression
+  {
+  public:
+    FTC2q2qbM();
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FTC2q2qbMFcon: public DoubleExpression
+  {
+  public:
+    FTC2q2qbMFcon();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC1q2qM: public DoubleExpression
+  {
+  public:
+    FLC1q2qM();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC1q2gM: public DoubleExpression
+  {
+  public:
+    FLC1q2gM();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC1g2qM: public DoubleExpression
+  {
+  public:
+    FLC1g2qM();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2qM: public DoubleExpression
+  {
+  public:
+    FLC2q2qM(int const& nf);
+    double RegularRegular(double const& x, double const& z) const override;
+  private:
+    int const _nf;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2qMFcon1: public DoubleExpression
+  {
+  public:
+    FLC2q2qMFcon1();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2qMFcon2: public DoubleExpression
+  {
+  public:
+    FLC2q2qMFcon2();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2qMANoW: public DoubleExpression
+  {
+  public:
+    FLC2q2qMANoW();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2gM: public DoubleExpression
+  {
+  public:
+    FLC2q2gM();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2gMANoW: public DoubleExpression
+  {
+  public:
+    FLC2q2gMANoW();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2g2qM: public DoubleExpression
+  {
+  public:
+    FLC2g2qM();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2g2qMANoW: public DoubleExpression
+  {
+  public:
+    FLC2g2qMANoW();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2g2gM: public DoubleExpression
+  {
+  public:
+    FLC2g2gM();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2g2gMAANoW: public DoubleExpression
+  {
+  public:
+    FLC2g2gMAANoW();
+    double LocalLocal(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2qpM1: public DoubleExpression
+  {
+  public:
+    FLC2q2qpM1();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2qpM2: public DoubleExpression
+  {
+  public:
+    FLC2q2qpM2();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2qpMNoW3: public DoubleExpression
+  {
+  public:
+    FLC2q2qpMNoW3();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2qpMNoW4: public DoubleExpression
+  {
+  public:
+    FLC2q2qpMNoW4();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2qbM: public DoubleExpression
+  {
+  public:
+    FLC2q2qbM();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class FLC2q2qbMFcon: public DoubleExpression
+  {
+  public:
+    FLC2q2qbMFcon();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C0q2qM: public DoubleExpression
+  {
+  public:
+    F3C0q2qM();
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C1q2qM: public DoubleExpression
+  {
+  public:
+    F3C1q2qM();
+    double LocalLocal(double const& x, double const& z) const override;
+    double LocalSingular(double const& x, double const& z) const override;
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularLocal(double const& x, double const& z) const override;
+    double SingularSingular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C1q2gM: public DoubleExpression
+  {
+  public:
+    F3C1q2gM();
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C1g2qM: public DoubleExpression
+  {
+  public:
+    F3C1g2qM();
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2q2qM: public DoubleExpression
+  {
+  public:
+    F3C2q2qM(int const& nf);
+    double LocalLocal(double const& x, double const& z) const override;
+    double LocalSingular(double const& x, double const& z) const override;
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularLocal(double const& x, double const& z) const override;
+    double SingularSingular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  private:
+    int const _nf;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2q2qMFcon1: public DoubleExpression
+  {
+  public:
+    F3C2q2qMFcon1();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2q2qMANoW: public DoubleExpression
+  {
+  public:
+    F3C2q2qMANoW();
+    double LocalLocal(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2q2gM: public DoubleExpression
+  {
+  public:
+    F3C2q2gM();
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2q2gMANoW: public DoubleExpression
+  {
+  public:
+    F3C2q2gMANoW();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2g2qM: public DoubleExpression
+  {
+  public:
+    F3C2g2qM();
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2g2qMANoW: public DoubleExpression
+  {
+  public:
+    F3C2g2qMANoW();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2q2qpM1: public DoubleExpression
+  {
+  public:
+    F3C2q2qpM1();
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2q2qpM2: public DoubleExpression
+  {
+  public:
+    F3C2q2qpM2();
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2q2qpMNoW3: public DoubleExpression
+  {
+  public:
+    F3C2q2qpMNoW3();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2q2qpMNoW4: public DoubleExpression
+  {
+  public:
+    F3C2q2qpMNoW4();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2q2qbM: public DoubleExpression
+  {
+  public:
+    F3C2q2qbM();
+    double LocalRegular(double const& x, double const& z) const override;
+    double SingularRegular(double const& x, double const& z) const override;
+    double RegularLocal(double const& x, double const& z) const override;
+    double RegularSingular(double const& x, double const& z) const override;
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+
+  /**
+   * @brief Description of the class
+   */
+  class F3C2q2qbMFcon: public DoubleExpression
+  {
+  public:
+    F3C2q2qbMFcon();
+    double RegularRegular(double const& x, double const& z) const override;
+  };
+}
